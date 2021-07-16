5a5190a905557c7a6aef5b9e98ea6b411676c110
FARUK ALAM HAWLADER <59347740+faruk-alam@users.noreply.github.com>
2021-07-16 20:01:44 +0200

    Add files via upload

---
+++ array intro-creating array/main.js
@@ -0,0 +1,16 @@
+// JavaScript array literal
+var colors = ["red","yellow","blue","purple","brown"];
+document.write(colors+"<br>");
+
+// JavaScript Array directly (new keyword)
+var cities = new Array();
+cities[0] = "New york";
+cities[1] = "London";
+cities[2] = "Amsterdam";
+cities[3] = "Berlin";
+document.write(cities+"<br>");
+
+//JavaScript array constructor (new keyword)
+var ages = new Array(54,7,23,32);
+document.write(ages);
+
