dd2fe09bb9f3cc4d92137d83d29001f6393ff542
FARUK ALAM HAWLADER <59347740+faruk-alam@users.noreply.github.com>
2021-07-17 13:08:50 +0200

    Add files via upload

---
+++ array-Accessing the Elements of an Array/main.js
@@ -0,0 +1,18 @@
+//Accessing the Elements of an Array
+var cities = new Array("London","Paris","Berlin","Amsterdam","New york");
+
+document.write(cities[2]+"<br>");
+document.write(cities[4]+"<br>");
+
+let grades = [];
+grades[0] = 23;
+grades[1] = 34;
+grades[2] = 77;
+grades[3] = 12;
+
+document.write(grades[3]+"<br>");
+
+
+
+
+
