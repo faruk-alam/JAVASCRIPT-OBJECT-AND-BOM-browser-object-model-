c8bc95b8271b00396fa87b40b845d1900292fdd9
FARUK ALAM HAWLADER <59347740+faruk-alam@users.noreply.github.com>
2021-07-17 18:44:35 +0200

    Add files via upload

---
+++ array-getting length and looping array/main.js
@@ -0,0 +1,24 @@
+//getting length
+var fruits = ["Apple", "Banana", "Mango", "Orange", "Papaya"];
+document.write(fruits.length+"<br>"); 
+//You can use for loop to access each element of an array in sequential order
+for(var i = 0;i < fruits.length;i++){
+    document.write(fruits[i]+" "+"<br>")
+}
+let grades = [];
+grades[0] = 33;
+grades[1] = 21;
+grades[2] = 11;
+grades[3] = 76;
+grades[4] = 20;
+grades[150] = 343;
+document.write(grades+"<br>");
+document.write(grades.length+" "+"<br>");
+for(var i = 0;i < fruits.length;i++){
+    document.write(grades[i]+" "+"<br>");
+}
+
+
+
+
+
