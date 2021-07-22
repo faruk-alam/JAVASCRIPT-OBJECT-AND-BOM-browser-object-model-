a7d080ec12133dd58619d025ecc433aab276098b
FARUK ALAM HAWLADER <59347740+faruk-alam@users.noreply.github.com>
2021-07-22 17:26:18 +0200

    Add files via upload

---
+++ array method-removing  element/main.js
@@ -0,0 +1,11 @@
+// using pop() method
+var colors = ["Red", "Green", "Blue", "Purple", "yellow"]
+document.write(colors+"<br/>");
+var last = colors.pop();
+document.write(last+"<br/>");
+document.write(colors+"<br/>");
+
+// using shift() method
+var dailyActivities = ['work', 'eat', 'sleep', 'exercise'];
+dailyActivities.shift();
+document.write(dailyActivities);
