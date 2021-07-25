2e1ca50d3eb3bd945d57fcaf7cde20eb76643f38
FARUK ALAM HAWLADER <59347740+faruk-alam@users.noreply.github.com>
2021-07-25 22:32:32 +0200

    Add files via upload

---
+++ array method-Creating a String from an Array/main.js
@@ -0,0 +1,8 @@
+// by using join():
+var fruits = ["apple", "banana", "mango", "banana"];
+console.log(fruits.join());
+console.log(fruits.join(""));
+console.log(fruits.join("-"));
+console.log(fruits.join(","));
+// using toString():
+console.log(fruits.toString());
