22cb944955eac1d7e2afcdb1133d795f41aced66
FARUK ALAM HAWLADER <59347740+faruk-alam@users.noreply.github.com>
2021-07-26 17:49:42 +0200

    Add files via upload

---
+++ array method-Extracting a Portion of an Array/main.js
@@ -0,0 +1,11 @@
+// using two parameters.
+
+var fruits = ["apple", "banana", "mango", "peach","pineapple","grape","orange"];
+var subarray = fruits.slice(1,4);
+console.log(subarray);
+// using one parameters.
+
+var fruits = ["apple", "banana", "mango", "peach","pineapple","grape","orange"];
+var subarray = fruits.slice(6);
+console.log(subarray);
+
