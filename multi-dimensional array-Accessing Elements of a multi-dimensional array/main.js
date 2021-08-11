5772fc6db1d6945671e95d0391598ff1a788413c
FARUK ALAM HAWLADER <59347740+faruk-alam@users.noreply.github.com>
2021-08-11 18:09:51 +0200

    Add files via upload

---
+++ multi-dimensional array-Accessing Elements of a multi-dimensional array/main.js
@@ -0,0 +1,9 @@
+let stuData = [["kalam",24],["jambo",40],["nahin",19]];
+// access the first item
+console.log(stuData[0]);
+
+//access the first item of the second inner array
+console.log(stuData[1][0]);
+
+//access the second item of the third inner array
+console.log(stuData[2][1]);
