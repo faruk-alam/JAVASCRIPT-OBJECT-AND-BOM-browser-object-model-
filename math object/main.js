01ad83e6a312d807df4235e863cbf81eeaef78ee
FARUK ALAM HAWLADER <59347740+faruk-alam@users.noreply.github.com>
2021-12-07 19:28:07 +0100

    Add files via upload

---
+++ math object/main.js
@@ -0,0 +1,53 @@
+// Printing PI value
+console.log(Math.PI);
+// Note: The Math object is a built-in JavaScript object so its properties and methods can be accessed directly. 
+// You'll never need to create a Math object, because it is automatically created by the JavaScript interpreter.
+
+// Printing the Absolute Value
+console.log(Math.abs(-1));
+console.log(Math.abs(-15.50));
+
+//Calculating the Square Root of a Number
+console.log(Math.sqrt(49));
+console.log(Math.sqrt(23));
+console.log(Math.sqrt(-30));
+
+// the round method
+console.log(Math.round(3.75));
+console.log(Math.round(5.07));
+console.log(Math.round(15.50));
+
+//The ceil() Method
+console.log(Math.ceil(2.70));
+console.log(Math.ceil(5.50));
+console.log(Math.ceil(-15.50));
+
+//The floor() Method
+console.log(Math.floor(2.30));
+console.log(Math.floor(5.50));
+console.log(Math.floor(-15.50));
+
+//Generating a Random Number
+console.log(Math.random());
+console.log(Math.random()*3);
+console.log(Math.random()*5);
+
+//Generating a integer Random Number
+console.log(Math.floor(Math.random()*7));
+console.log(Math.floor(Math.random()*9));
+console.log(Math.floor(Math.random()*5+1));
+
+//The Math.pow() method
+console.log(Math.pow(2,3));
+console.log(Math.pow(5,2));
+console.log(Math.pow(0,3));
+console.log(Math.pow(-5,3));
+console.log(Math.pow(5,-3));
+
+//The Math.max()
+console.log(Math.max(12,7,4,-9,50,3));
+console.log(Math.max(12,7,4,9,8,3));
+
+//The Math.min()
+console.log(Math.min(12,7,4,-9,50,3));
+console.log(Math.min(12,7,4,9,50,3));
